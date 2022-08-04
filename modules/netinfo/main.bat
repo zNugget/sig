995a1e6f844f486f03dc9c273aa50379463fb7d6
zNugget <81307155+zNugget@users.noreply.github.com>
2022-08-04 16:46:23 -0400

    Create main.bat

---
+++ modules/netinfo/main.bat
@@ -0,0 +1,10 @@
+@echo off
+@title test
+
+:: Netinfo - A network info module for sig.
+:: Publisher: zNugget
+:: (No variables are required for input.)
+
+ipconfig /all
+netstat
+pause >nobreak >null
