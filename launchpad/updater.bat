9c7ab7da3348c2f663cd2875cfd1c422f0745541
zNugget <81307155+zNugget@users.noreply.github.com>
2022-08-04 16:20:49 -0400

    Delete updater.bat

--- launchpad/updater.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-del sig.bat
-for /F %%I in ('curl https://raw.githubusercontent.com/zNugget/sig/main/launchpad/sig.txt') do set sigupdate=%%I >nobreak >nul && cls
-echo %sigupdate%> sig.bat
-sig.bat
