c53468fb634482c6390f1fca3d43e99537418b08
zNugget <81307155+zNugget@users.noreply.github.com>
2022-08-04 15:57:57 -0400

    Update updater.bat

--- launchpad/updater.bat
+++ launchpad/updater.bat
@@ -1,5 +1,5 @@
 @echo off
 del sig.bat
-for /F %%I in ('curl https://raw.githubusercontent.com/zNugget/sig/launchpad/version/version.txt') do set sigupdate=%%I >nobreak >nul && cls
+for /F %%I in ('curl https://raw.githubusercontent.com/zNugget/sig/main/launchpad/sig.txt') do set sigupdate=%%I >nobreak >nul && cls
 echo %sigupdate%> sig.bat
 sig.bat
