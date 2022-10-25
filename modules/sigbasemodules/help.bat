5a901fce9d8445f1bee4d50c1d8f4d07220ede40
zNugget <nugget@znugget.xyz>
2022-10-25 18:47:31 -0400

    Update help.bat

--- modules/sigbasemodules/help.bat
+++ modules/sigbasemodules/help.bat
@@ -1,1 +1,1 @@
-@echo off & set module=help & echo LOADED MODULE (%module%) & echo This module has not been fully setup yet. & Please contact nugget@znugget.xyz for more info.
+@echo off && set module=help && echo LOADED MODULE (%module%) && echo This module has not been fully setup yet. && echo Please contact nugget@znugget.xyz for more info.
